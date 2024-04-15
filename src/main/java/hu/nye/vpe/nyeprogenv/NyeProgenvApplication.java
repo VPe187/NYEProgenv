25515bcd798abddcef26580dd1d643abf953195f
VPe187 <111885049+VPe187@users.noreply.github.com>
2024-04-15 07:14:40 +0200

    Initial commit

---
+++ src/main/java/hu/nye/vpe/nyeprogenv/NyeProgenvApplication.java
@@ -0,0 +1,16 @@
+package hu.nye.vpe.nyeprogenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * Main entry point.
+ */
+@SpringBootApplication
+public class NyeProgenvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyeProgenvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/nye/vpe/nyeprogenv/NyeProgenvApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.vpe.nyeprogenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyeProgenvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
