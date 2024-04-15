74b383955103544c20b2c1f53591a01f683513cc
VPe187 <111885049+VPe187@users.noreply.github.com>
2024-04-15 21:25:45 +0200

    First attempt integrate tailwind.

--- src/main/java/hu/nye/vpe/nyeprogenv/MainController.java
+++ src/main/java/hu/nye/vpe/nyeprogenv/MainController.java
@@ -3,6 +3,9 @@ package hu.nye.vpe.nyeprogenv;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+/* Main controller.
+**/
 @Controller
 public class MainController {
 
